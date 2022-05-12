768cb44959c20b41d3577793e345a9574c982307
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-05-13 05:17:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home/Home';
+import Header from './component/Share/Header';
+import About from './component/About/About';
+import Appointment from './component/Appointment/Appointment';
+import Contact from './component/Contact/Contact';
+import LogIn from './component/LogIn/LogIn';
+import Register from './component/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/log-in' element={<LogIn></LogIn>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Registere</h1>
+        </div>
+    );
+};
+
+export default Register;
